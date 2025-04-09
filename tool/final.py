8ee31716507a1c34a2ce14f0f9b0d89c32920762
Siddhant Kumar Sahu <catchmesiddhant@gmail.com>
2025-04-09 22:50:52 +0530

    cache commit

--- tool/final.py
+++ tool/final.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
 st.set_page_config(page_title="BTD Visualization Tool", layout="wide")
 
@@ -38,3 +39,25 @@ df['daily_sales'] = df['transaction_qty'].resample('D').sum()
 df['hourly_avg'] = df.groupby('Hour')['transaction_qty'].mean()
 
 # Plots for my visualization tool
+
+st.markdown("### 1. Footfall of customers by Hour")
+
+# Set consistent day order
+day_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+# Add day name column
+df['Day_name'] = df.index.day_name()
+
+# Count number of transactions per date
+daily_counts = df.groupby([df.index.date, 'Day_name'])['transaction_id'].count().reset_index()
+daily_counts.columns = ['Date', 'Day_name', 'Bills']
+
+# Average per weekday
+avg_daily_footfall = daily_counts.groupby('Day_name')['Bills'].mean().reindex(day_order)
+
+# Plot
+col1, col2, col3 = st.columns
+
+
+fig = px.bar(x=avg_daily_footfall.index, y=avg_daily_footfall.values, title="Footfall by Day")
+st.plotly_chart(fig, use_container_width=True)
